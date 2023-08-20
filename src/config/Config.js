56adfd8eb9df8f979d8f05d31c757e7f6bedc0a1
Eric Tran <etra8011@uni.sydney.edu.au>
2023-08-20 19:46:28 +1000

    Intiated codebase with project structure, gameMenu and basic button functionality

---
+++ src/config/Config.js
@@ -0,0 +1,33 @@
+class Config {
+    static get ASSET_PATH() {
+        return 'assets/';
+    }
+
+    static get FRAME_WIDTH() {
+        return 16;
+    }
+
+    static get FRAME_HEIGHT() {
+        return 16;
+    }
+
+    // static get SPRITE_FILE() {
+    //     return 'Toens_Medieval_v.1.0';
+    // }
+
+    static get SPRITE_EXT() {
+        return '.png';
+    }
+
+    static get WindowWidth() {
+        return 50 * 16;
+    }
+
+    static get WindowHeight() {
+        return 50 * 16;
+    }
+
+    static get DialogTransitionTime() {
+        return 1000;
+    }
+}
