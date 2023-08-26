6713e4005ff648a94f98a9e3ca9a0907e218ee5a
Jerome <jaya9137@uni.sydney.edu.au>
2023-08-26 15:55:14 +1000

    gg

--- src/scene/Personnel.js
+++ src/scene/Personnel.js
@@ -16,17 +16,45 @@ class Personnel extends Phaser.Scene {
         this.load.image('button1Hover', 'assets/buttons/button_hover.png');
 
         this.load.image('cardBackground', 'assets/cards/card3/Card X5.png');
+        this.load.image('taskCard', 'assets/cards/card1/Card X1.png');
         this.load.image('panel', 'assets/cards/card1/Panel Empty Green.png');
         this.load.image('background', 'assets/backgrounds/background1.png');
     }
 
     create() {
         const BOX_Y_POS = 100;
+        const TASK_1_X_POS = 130;
+        const TASK_2_X_POS = 330;
+        const TASK_3_X_POS = 530;
+        const TASK_4_X_POS = 730;
+
+        const TASK_Y_POS = 500;
+
 
         this.add.image(Config.WindowWidth / 2, Config.WindowHeight / 2, 'background');
         const card = this.add.image(this.cameras.main.width / 2, BOX_Y_POS, 'cardBackground').setScale(1.1, 0.5);
-        const cardText = this.add.text(card.x - card.width / 2 + 10, card.y - card.height / 2 + 10, 'Card Title', { fontSize: '20px', color: '#FFFFFF' }).setDepth(2);
-        cardText.setOrigin(50, 50);
+
+        const cardText = this.add.text(75, BOX_Y_POS-70, 'Available Personnel', {
+             fontSize: 20, 
+             color: '#ffffff' 
+            });
+        //card.add(cardText);
+        cardText.setOrigin(0,0);
+
+        const task1 = this.add.image(TASK_1_X_POS, TASK_Y_POS, 'taskCard').setScale(0.5, 0.7);
+        const task2 = this.add.image(TASK_2_X_POS, TASK_Y_POS, 'taskCard').setScale(0.5, 0.7);
+        const task3 = this.add.image(TASK_3_X_POS, TASK_Y_POS, 'taskCard').setScale(0.5, 0.7);
+        //const task4 = this.add.image(TASK_4_X_POS, TASK_Y_POS, 'taskCard').setScale(0.5, 0.7);
+
+        const cardTextTask1 = this.add.text(TASK_1_X_POS - 40 , TASK_Y_POS-225, 'Task 1', { fontSize: '20px', color: '#ffffff' });
+        cardTextTask1.setOrigin(0, 0);
+    
+        const cardTextTask2 = this.add.text(TASK_2_X_POS - 40 , TASK_Y_POS-225, 'Task 2', { fontSize: '20px', color: '#ffffff' });
+        cardTextTask2.setOrigin(0, 0);
+    
+        const cardTextTask3 = this.add.text(TASK_3_X_POS - 40, TASK_Y_POS-225, 'Task 3', { fontSize: '20px', color: '#ffffff' });
+        cardTextTask3.setOrigin(0, 0);
+
         // Create snap positions
         const snapPositions = [200, 400, 600];
 
