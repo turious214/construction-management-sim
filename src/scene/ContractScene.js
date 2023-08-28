0319405f38b58e8db8feeac27479996793160b3e
Kevin Zhou <jzho7657@sydney.edu.au>
2023-08-28 19:31:00 +1000

    UI fixed, touchable container

--- src/scene/ContractScene.js
+++ src/scene/ContractScene.js
@@ -36,201 +36,92 @@ class ContractScene extends Phaser.Scene {
         this.add.image(Config.WindowWidth / 2, Config.WindowHeight / 2, 'background');
 
         // add exist button
-        const existButton = new CustomButton(this, INIT_MAIN_UI_X * 11, INIT_MAIN_UI_Y * 36, 'button1Normal', 'button1Hover', 'Exist');
+        const existButton = new CustomButton(this, INIT_MAIN_UI_X * 8, INIT_MAIN_UI_Y * 20,'button1Normal', 'button1Hover', 'Exist', 30);
         this.add.existing(existButton);
+        existButton.setDepth(1);
 
         // go main scene
         existButton.setInteractive()
             .on(Phaser.Input.Events.GAMEOBJECT_POINTER_DOWN, () => {
-                this.scene.stop('Contract');
+                this.scene.stop('ContractScene');
                 this.scene.launch('MainScene');
             });
 
         // add title
         this.add.text(Config.WindowWidth / 16, Config.WindowHeight / 16, 'CONTRACT', {
             color: '#fcd498',
-            fontSize: 50,
+            fontSize: 100,
             align: 'top',
-        }).setFixedSize(450, 50);
+        });
 
         // add card
-        const card = this.add.image(400, 400, 'card').setScale(6, 6);
+        const card = this.add.image(INIT_MAIN_UI_X * 4.8, INIT_MAIN_UI_Y * 11.5, 'card').setScale(2.5, 2);
 
         // add scroll view
-        this.scrollView = this.add.container(card.x / 7, card.y / 4);
-        const scrollViewContent = null;
+        this.scrollView = this.add.container(INIT_MAIN_UI_X * 1.05, INIT_MAIN_UI_Y * 6);
 
         // put random info[default = 20 messages]
         for (let i = 0; i < 20; i++) {
-            const scrollViewContent = this.add.container(this.scrollView.x , this.scrollView.y / 4 * i + 76 + i * 35);
+            // add each contract
+            const scrollViewContent = this.add.container(this.scrollView.x / 10, this.scrollView.y / 6 * i);
             this.scrollView.add(scrollViewContent);
 
             // name
             const generateName = generateRandomCompanyName();
-            const companyName = this.add.text(0, 0, generateName, {
-                fontSize: 28,
+            const companyName = this.add.text(scrollViewContent.x, scrollViewContent.y, generateName, {
+                fontSize: 40,
                 color: '#ffffff'
             });
             scrollViewContent.add(companyName);
 
             // rating
             const generateRating = generateRandomRating();
-            const ratingImage5 = this.add.image(400, 8.5,  generateRating)
-                .setScale(0.04, 0.04)
-            
-            // make stars open window
-
-            // ratingImage5.setInteractive()
-            // .on(Phaser.Input.Events.GAMEOBJECT_POINTER_DOWN, () => {
-            //     this.scene.stop('ContractSceme');
-            //     this.scene.start('MainScene');
-            // });
-            // scrollViewContent.add(ratingImage5);
-
-            //   make stars open window
-
-            ratingImage5.setInteractive()
-            .on(Phaser.Input.Events.GAMEOBJECT_POINTER_DOWN, function () {
-                this.createWindow(TaskAssignmentScene)
-                console.log('new window created')
-            }, this);
-            scrollViewContent.add(ratingImage5);
-
+            const ratingImage = this.add.image(scrollViewContent.x + 1000, scrollViewContent.y + 18,  generateRating)
+                .setScale(0.1, 0.1)
+            scrollViewContent.add(ratingImage);
 
             // price
-            const generatePrice = generateRandomPrice(ratingImage5.texture.key);
-            const price = this.add.text(475, 1, `$${generatePrice}`, {
-                fontSize: 18,
+            const generatePrice = generateRandomPrice(ratingImage.texture.key);
+            const price = this.add.text(scrollViewContent.x + 1200, scrollViewContent.y+3, `$${generatePrice}`, {
+                fontSize: 40,
                 color: '#ffffff'
             });
             scrollViewContent.add(price);
+
+            // add each container's touchable area
+            const clickArea = this.add.graphics();
+            clickArea.fillStyle(0xff0000);
+            clickArea.fillRect(scrollViewContent.x, scrollViewContent.y - 10, 1380, 60);
+            clickArea.setAlpha(0.5);
+            clickArea.setDepth(-1);
+            scrollViewContent.add(clickArea);
+
+            // set each clickArea open window
+            const areaCheck1 = new Phaser.Geom.Rectangle(scrollViewContent.x, scrollViewContent.y - 10, 1380, 60)
+            const areaCheck2 = new Phaser.Geom.Rectangle(INIT_MAIN_UI_X * 1.05 + 40, INIT_MAIN_UI_Y * 6 - 10, 1382, 571)
+
+            clickArea.setInteractive(areaCheck1, Phaser.Geom.Rectangle.Contains)
+                .on(Phaser.Input.Events.GAMEOBJECT_POINTER_DOWN, function (pointer) {
+                    // unsure click in two area
+                    if (Phaser.Geom.Rectangle.Contains(areaCheck2, pointer.x, pointer.y)) {
+                        console.log(companyName.text);
+                        this.createWindow(TaskAssignmentScene);
+                    }
+                }, this);
 
             // add scroll view mask
             const mask = this.make.graphics();
             mask.fillStyle(0xffffff);
             mask.fillRect(0, -1000, 1000, 1000);
             mask.fillStyle(0x000000);
-            mask.fillRect(100, 150, 800, 500)
+            mask.fillRect(INIT_MAIN_UI_X * 1.05 + 40, INIT_MAIN_UI_Y * 6 - 10, 1382, 571)
             scrollViewContent.setMask(mask.createGeometryMask());
         }
 
         // add scroll bar
         this.scrollbar = this.add.graphics();
         this.scrollbar.fillStyle(0x666666, 1);
-        this.scrollbar.fillRect(680, 175, 30, 50);
-
-
-
-        // const redPanel = this.add.image(0, 0, 'red-panel');
-
-        // // outermost container
-        // const contentContainer = this.add.container(400, 400, [redPanel]);
-        // contentContainer.setSize(redPanel.width, redPanel.height);
-        // contentContainer.setInteractive()
-        //     .on(Phaser.Input.Events.GAMEOBJECT_POINTER_DOWN, () => {
-        //         this.scene.stop('ContractSceme');
-        //         this.scene.start('MainScene');
-        //     });
-
-        // this.input.setDraggable(contentContainer);
-
-        // contentContainer.setInteractive(new Phaser.Geom.Rectangle(0, 0, 400, 400), Phaser.Geom.Rectangle.Contains);
-
-        // contentContainer.on('drag', function (pointer, dragX, dragY) {
-
-        //     this.x = dragX;
-        //     this.y = dragY;
-
-        // });
-
-        this.input.on('drag', (pointer, gameObject, dragX, dragY) =>
-        {
-
-            gameObject.x = dragX;
-            gameObject.y = dragY;
-
-        });
-
-        
-
-
-
-
-
-        // // add scroll view
-        // this.scrollView = this.add.container(card.x / 7, card.y / 4);
-        // const scrollViewContent = null;
-
-        // // put random info[default = 20 messages]
-        // for (let i = 0; i < 20; i++) {
-        //     const scrollViewContent = this.add.container(this.scrollView.x , this.scrollView.y / 4 * i + 76 + i * 35);
-        //     this.scrollView.add(scrollViewContent);
-
-        //     // name
-        //     const generateName = generateRandomCompanyName();
-        //     const companyName = this.add.text(0, 0, generateName, {
-        //         fontSize: 28,
-        //         color: '#ffffff'
-        //     });
-        //     scrollViewContent.add(companyName);
-
-        //     // rating
-        //     const generateRating = generateRandomRating();
-        //     const ratingImage5 = this.add.image(400, 8.5,  generateRating)
-        //         .setScale(0.04, 0.04)
-        //     scrollViewContent.add(ratingImage5);
-
-        //     // price
-        //     const generatePrice = generateRandomPrice(ratingImage5.texture.key);
-        //     const price = this.add.text(475, 1, `$${generatePrice}`, {
-        //         fontSize: 18,
-        //         color: '#ffffff'
-        //     });
-        //     scrollViewContent.add(price);
-
-        //     // add scroll view mask
-        //     const mask = this.make.graphics();
-        //     mask.fillStyle(0xffffff);
-        //     mask.fillRect(0, -1000, 1000, 1000);
-        //     mask.fillStyle(0x000000);
-        //     mask.fillRect(100, 150, 800, 500)
-        //     scrollViewContent.setMask(mask.createGeometryMask());
-        // }
-
-        // // add scroll bar
-        // this.scrollbar = this.add.graphics();
-        // this.scrollbar.fillStyle(0x666666, 1);
-        // this.scrollbar.fillRect(680, 175, 30, 50);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        // this.input.setDraggable(this.scrollView)
-
-        // this.scrollView.on('drag', function (pointer, dragX, dragY) {
-
-        //     this.x = dragX;
-        //     this.y = dragY;
-
-        // });
-
+        this.scrollbar.fillRect(INIT_MAIN_UI_X * 8.5, INIT_MAIN_UI_Y * 5.8, 30, 50);
     }
 
     update() {
@@ -241,9 +132,8 @@ class ContractScene extends Phaser.Scene {
             this.scene.launch('MainScene');
         }
 
-
         // set scroll bar
-        this.scrollbar.setInteractive(new Phaser.Geom.Rectangle(680, 175, 30, 50), Phaser.Geom.Rectangle.Contains);
+        this.scrollbar.setInteractive(new Phaser.Geom.Rectangle(this.scrollbar.commandBuffer[4], this.scrollbar.commandBuffer[5], 30, 50), Phaser.Geom.Rectangle.Contains);
 
         this.scrollbar.on('pointerdown', () => {
             this.isDragging = true;
@@ -257,40 +147,29 @@ class ContractScene extends Phaser.Scene {
         if (this.isDragging) {
             const pointer = this.input.activePointer;
 
-            const offsetY = pointer.y - 200;
-            this.scrollbar.y = Phaser.Math.Clamp(offsetY, 0, 400);
+            const offsetY = pointer.y - this.scrollbar.commandBuffer[5];
+            this.scrollbar.y = Phaser.Math.Clamp(offsetY, 0, 525);
 
-            //51.5*n+n/10 scrollView height - scrollbar height = scroll speed
-            const contentY = (this.scrollbar.y / (400 - 50)) * (1030 - 400) - 100;
+            //set scroll speed
+            const contentY = (this.scrollbar.y / 1100) * 2910 - 300;
             this.scrollView.y = -contentY;
-            console.log(this.scrollView.y);
         }
     }
 
-    
     createWindow (func)
     {
         var handle = 'window' + this.count++;
-
         var win = this.add.zone(400, 400, func.WIDTH, func.HEIGHT).setInteractive().setOrigin(0);
-
         var demo = new func(handle, win);
-
         this.input.setDraggable(win);
 
         win.on('drag', function (pointer, dragX, dragY) {
-
             this.x = dragX;
             this.y = dragY;
-
             demo.refresh()
-
         });
-
         this.scene.add(handle, demo, true);
     }
-
-
 
 }
 
