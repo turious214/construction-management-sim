23315badad55a3201c39076c60f6043555796a44
adrian <adrianlin92@gmail.com>
2023-09-12 01:40:58 +1000

    filter function but position hardcoded currently

--- src/scene/Materials.js
+++ src/scene/Materials.js
@@ -17,6 +17,7 @@ class Materials extends Phaser.Scene {
 
         this.load.image('background', 'assets/backgrounds/background1.png');
         this.load.image('card', 'assets/cards/card3/Card X5.png')
+        this.load.scenePlugin('rexuiplugin', 'https://raw.githubusercontent.com/rexrainbow/phaser3-rex-notes/master/dist/rexuiplugin.min.js', 'rexUI', 'rexUI');
     }
 
     create() {
@@ -96,12 +97,106 @@ class Materials extends Phaser.Scene {
         this.scrollbar.fillStyle(0x666666, 1);
         this.scrollbar.fillRect(1380, 315, 20, 50);
 
-        const filterButton= new CustomButton(this, 100, 100, 'filter_up', 'filter_down', 30);
-        this.add.existing(filterButton);
-        filterButton.setInteractive()
-            .on(Phaser.Input.Events.GAMEOBJECT_POINTER_DOWN, () => {
-                filter(this.scrollView);
-            });
+
+        const COLOR_PRIMARY = 0x4e342e;
+        const COLOR_LIGHT = 0x7b5e57;
+        const COLOR_DARK = 0x260e04;
+
+        var dropDownList = this.rexUI.add.dropDownList({
+            x: 1250, y: 350,
+
+            background: this.rexUI.add.roundRectangle(0, 0, 2, 2, 0, COLOR_PRIMARY),
+            text: CreateTextObject(this, 'Sort by').setFixedSize(150, 0),
+
+            //padding
+            space: {
+                left: 10,
+                right: 10,
+                top: 10,
+                bottom: 10,
+                icon: 10
+            },
+
+            //drop down menu options
+            options: [
+                { text: 'Ascend Alphabetical' },
+                { text: 'Descend Alphabetical' },
+                { text: 'Ascend Numerical' },
+                { text: 'Descend Numerical' },
+            ],
+
+        list: {
+                createBackgroundCallback: function (scene) {
+                    return scene.rexUI.add.roundRectangle(0, 0, 2, 2, 0, COLOR_DARK);
+                },
+                createButtonCallback: function (scene, option, index, options) {
+                    var text = option.text;
+                    var button = scene.rexUI.add.label({
+                        background: scene.rexUI.add.roundRectangle(0, 0, 2, 2, 0),
+
+                        //text box
+                        text: CreateTextObject(scene, text),
+
+                        //padding
+                        space: {
+                            left: 10,
+                            right: 10,
+                            top: 10,
+                            bottom: 10,
+                            icon: 10
+                        }
+                    });
+                    button.text= option.text;
+
+                    var alpha_num;
+                    var asc_des;
+
+                    //set variables for filter based on case
+                    switch(button.text) {
+                        case "Ascend Alphabetical":
+                            alpha_num = true;
+                            asc_des = true;
+                            break;
+                        case "Descend Alphabetical":
+                            alpha_num = true;
+                            asc_des = false;
+                            break;
+                        case "Ascend Numerical":
+                            alpha_num = false;
+                            asc_des = true;
+                            break;
+                        case "Descend Numerical":
+                            alpha_num = false;
+                            asc_des = false;
+                            break;
+                        default:
+                            alpha_num = true;
+                            asc_des = true;
+                            break;
+                    }
+
+                    //make button filter based on selection
+                    button.setInteractive()
+                        .on(Phaser.Input.Events.GAMEOBJECT_POINTER_DOWN, () => {
+                            filter(scene.scrollView, alpha_num, asc_des);
+                    });
+                
+                    return button;
+                },
+
+                // scope: dropDownList
+                onButtonOver: function (button, index, pointer, event) {
+                    button.getElement('background').setStrokeStyle(1, 0xffffff);
+                },
+
+                // scope: dropDownList
+                onButtonOut: function (button, index, pointer, event) {
+                    button.getElement('background').setStrokeStyle();
+                },
+            },
+            value: undefined
+
+        }).layout();
     }
 
     update() {
@@ -130,9 +225,17 @@ class Materials extends Phaser.Scene {
     }
 }
 
+function CreateTextObject(scene, text) {
+    return scene.add.text(0, 0, text, { fontSize: 20 })
+}
+
 //also add parameter for increase or decrease
-function filter(scrollView){
-    var i, shouldSwitch;
+function filter(scrollView, alpha_num, asc_des){
+    var i;
+    var shouldSwitch;
+    var valueCmp;
+    var valueCmpOther;
+    var alpha = alpha_num ? 0 : 1;
     listView = scrollView.list;
     switching = true;
 
@@ -140,10 +243,24 @@ function filter(scrollView){
         switching = false;
         for (i = 0; i < listView.length - 1; i++) {
             shouldSwitch = false;
-            //have switch statement for numbers and asc and desc
-            if(listView[i].list[0].text > listView[i + 1].list[0].text) {
-                shouldSwitch = true;
-                break;
+            valueCmp = listView[i].list[alpha].text;
+            valueCmpOther = listView[i+1].list[alpha].text;
+
+            if(alpha) {
+                valueCmp = parseInt(valueCmp);
+                valueCmpOther = parseInt(valueCmpOther);
+            }
+
+            if(asc_des) {
+                if(valueCmp > valueCmpOther) {
+                    shouldSwitch = true;
+                    break;
+                }
+            } else {
+                if(valueCmp < valueCmpOther) {
+                    shouldSwitch = true;
+                    break;
+                }
             }
         }
 
