367df0d544344c2ac013491e8953cc995b2e77a1
Eric Tran <etra8011@uni.sydney.edu.au>
2023-09-19 09:41:16 +1000

    Added property-based-testing for materials

---
+++ test/jest/Materials.test.ts
@@ -0,0 +1,69 @@
+import {expect} from '@jest/globals';
+import Material from '../../src/gameobject/material/Material';
+import {fc, test} from '@fast-check/jest';
+import MaterialCategory from "../../src/gameobject/material/MaterialCategory";
+
+describe ('Material name PBT', () => {
+    test('valid string name', () => {
+        const material: Material = new Material('a', MaterialCategory.HVAC, 0);
+
+        fc.assert(
+            fc.property(fc.asciiString(), s => {
+
+                // recast to String
+                const testName: string = String(s)
+                material.name = testName;
+                expect(material.name).toEqual(testName);
+            })
+        );
+    });
+});
+
+describe ('Material category', () => {
+    const material: Material = new Material('a', MaterialCategory.HVAC, 0);
+    test('MaterialCategory CONSTRUCTION', () => {
+        const cat = MaterialCategory.CONSTRUCTION;
+        material.category = cat;
+        expect(material.category).toEqual(cat);
+    });
+    test('MaterialCategory ELECTRICAL', () => {
+        const cat = MaterialCategory.ELECTRICAL;
+        material.category = cat;
+        expect(material.category).toEqual(cat);
+    });
+    test('MaterialCategory PLUMBING', () => {
+        const cat = MaterialCategory.PLUMBING;
+        material.category = cat;
+        expect(material.category).toEqual(cat);
+    });
+    test('MaterialCategory LANDSCAPING', () => {
+        const cat = MaterialCategory.LANDSCAPING;
+        material.category = cat;
+        expect(material.category).toEqual(cat);
+    });
+    test('MaterialCategory HVAC', () => {
+        const cat = MaterialCategory.HVAC;
+        material.category = cat;
+        expect(material.category).toEqual(cat);
+    });
+});
+
+
+describe ('Material price PBT', () => {
+    test('valid price range', () => {
+        const material: Material = new Material('a', MaterialCategory.HVAC, 0);
+
+        fc.assert(
+            fc.property(fc.double(), x => {
+
+                // recast to Number
+                const testPrice: number = Number(x)
+                material.price = testPrice;
+                expect(material.price).toEqual(testPrice);
+            })
+        );
+    });
+});
+
+
+
