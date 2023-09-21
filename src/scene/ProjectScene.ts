1eaf3a0ca0ddc6a0125bd0fe3a3d74e14a55c7f8
Jerome <jaya9137@uni.sydney.edu.au>
2023-09-21 19:03:31 +1000

    added swideways scroll and popup logic

--- src/scene/ProjectScene.ts
+++ src/scene/ProjectScene.ts
@@ -11,6 +11,8 @@ export default class ProjectScene extends Phaser.Scene {
     private funds: number;
     private tasks: Task[][];
     private popupContainer?: Phaser.GameObjects.Container;
+
+    private taskButtonContainers: Phaser.GameObjects.Container[] = [];
     
     constructor() {
         super('ProjectScene');
@@ -55,6 +57,9 @@ export default class ProjectScene extends Phaser.Scene {
         this.load.image('taskCard', 'assets/cards/card3/Card X5.png');
         //this.load.image('panel', 'assets/cards/card1/Panel Empty Green.png');
         this.load.image('background', 'assets/backgrounds/background1.png');
+
+        this.load.image('leftArrow', 'assets/icons/09.png');
+        this.load.image('rightArrow', 'assets/icons/10.png');
         
     }
 
@@ -62,48 +67,94 @@ export default class ProjectScene extends Phaser.Scene {
         
         
         this.add.image(Config.WindowWidth / 2, Config.WindowHeight / 2, 'background');
-        //this.cameras.main.setBounds(0, 0, Config.WindowWidth, Config.WindowHeight);
         const TASK_SCALE_FACTOR = 0.5;
         const TASK_X_START_POS = 200;
         const XPOS_DIFF_FACTOR = 450;
         const TASK_Y_START_POS = 300;
         const YPOS_DIFF_FACTOR = 300;
 
+        this.taskButtonContainers = [];
         for (let i = 0; i < this.tasks.length; i++) {
             for (let j = 0; j < this.tasks[i].length; j++) {
                 const task = this.tasks[i][j];
-                
-                // Create a task button as an image
-                const taskButton = this.add.image(
+        
+                // Create a container for the task button
+                const taskButtonContainer = this.add.container(
                     TASK_X_START_POS + XPOS_DIFF_FACTOR * i,
-                    TASK_Y_START_POS + YPOS_DIFF_FACTOR * j,
-                    'taskCard'
-                ).setScale(TASK_SCALE_FACTOR, TASK_SCALE_FACTOR);
-                
-                this.add.text(TASK_X_START_POS + XPOS_DIFF_FACTOR * i - 110, TASK_Y_START_POS + YPOS_DIFF_FACTOR * j - 30, task.Title, { fontSize: '70px', color: '#ffffff' }).setOrigin(0,0);
-                
-                // Set interactive to make it clickable
-                taskButton.setInteractive();
-
-                // Define a callback function when the button is clicked
-                taskButton.on('pointerdown', () => {
+                    TASK_Y_START_POS + YPOS_DIFF_FACTOR * j
+                );
+        
+                // Create a task button as an image
+                const taskButtonImage = this.add.image(0, 0, 'taskCard');
+                taskButtonImage.setScale(TASK_SCALE_FACTOR, TASK_SCALE_FACTOR);
+                taskButtonContainer.add(taskButtonImage);
+        
+                // Set the size of the container to match the task button image
+                taskButtonContainer.setSize(taskButtonImage.width, taskButtonImage.height);
+        
+                // Create text and add it to the container
+                const taskText = this.add.text(-110, -50, task.Title, { fontSize: '70px', color: '#ffffff', wordWrap: { width: 10 } }).setOrigin(0, 0);
+                taskButtonContainer.add(taskText);
+        
+                // Set the container as interactive
+                taskButtonContainer.setInteractive();
+        
+                // Define a callback function when the container is clicked
+                taskButtonContainer.on('pointerdown', () => {
                     // Handle the task button click here, e.g., show task details
-                    console.log(`Task ${i + 1}-${j + 1} clicked.`);
-
+                    console.log(`Task ${i + 1}-${j + 1} (${task.Title}) clicked.`);
                     this.openPopup(task);
                 });
+        
+                // Give the container a unique name for later reference
+                taskButtonContainer.setName(`taskButtonContainer_${i}_${j}`);
+                this.taskButtonContainers.push(taskButtonContainer);
             }
         }
+
+        const leftArrowButton = this.add.image(50, 50, 'leftArrow');
+        leftArrowButton.setInteractive();
+        leftArrowButton.on('pointerdown', () => {
+            // Shift the taskButton container to the right by the width of a task button
+            console.log(`left clicked.`);
+            this.shiftTaskButtons(XPOS_DIFF_FACTOR);
+        });
+
+        // Create right arrow button
+        const rightArrowButton = this.add.image(Config.WindowWidth - 50, 50, 'rightArrow');
+        rightArrowButton.setInteractive();
+        rightArrowButton.on('pointerdown', () => {
+            // Shift the taskButton container to the left by the width of a task button
+            console.log(`right clicked.`);
+            this.shiftTaskButtons(-XPOS_DIFF_FACTOR);
+        });
     }
 
     update() {
     }
 
+    private shiftTaskButtons(shiftAmount: number): void {
+        //set the bounds of horizontal scroll
+        if (this.taskButtonContainers[this.taskButtonContainers.length - 1].x + shiftAmount < 200){
+            return;
+        }
+        if (this.taskButtonContainers[0].x + shiftAmount > 200){
+            return;
+        }
+
+        for (let i = 0; i < this.taskButtonContainers.length; i++) {
+            // Get the current taskButtonContainer
+            const taskButtonContainer = this.taskButtonContainers[i];
+    
+            // Update the x position of the container by the shiftAmount
+            taskButtonContainer.x += shiftAmount;
+            
+        }
+    }
+
     private openPopup(task: Task): void {
 
         if (this.popupContainer) {
-            // If a popup is open, close it before opening a new one
-            //this.closePopup();
             return;
         }
 
@@ -111,39 +162,60 @@ export default class ProjectScene extends Phaser.Scene {
         this.popupContainer = this.add.container(Config.WindowWidth / 2, Config.WindowHeight / 2);
     
         // Create an image and add it to the popup container
-        const popupImage = this.add.image(0, 0, 'taskCard'); // Replace 'taskCard' with the key of your image asset
+        const popupImage = this.add.image(0, 0, 'taskCard'); 
         popupImage.setScale(2.5); // Adjust the scale as needed
         this.popupContainer.add(popupImage);
 
-        const popupBackground = this.add.rectangle(0, 0, 400, 200, 0x000000, 0.8);
-        this.popupContainer.add(popupBackground);
+        // Set the size of the container to match the popupImage
+        this.popupContainer.setSize(popupImage.width*2.5, popupImage.height*2.5);
+
+        // Make the popup container non-clickable through
+        this.popupContainer.setInteractive({ inputEnabled: true });
+
+        //const popupBackground = this.add.rectangle(0, 0, 400, 200, 0x000000, 0.8);
+        //this.popupContainer.add(popupBackground);
     
         // Create text to overlay on the image
-        const popupText = this.add.text(0, 0, task.description, {
-            fontSize: '24px',
+        const TaskDescriptionText = this.add.text(0, 0, task.description, {
+            fontSize: '48px',
             color: '#ffffff',
-            wordWrap: { width: 360 },
+            wordWrap: { width: popupImage.width },
         });
-        popupText.setOrigin(1, 0.5); // Center the text on the image
-        this.popupContainer.add(popupText);
-    
+        TaskDescriptionText.setOrigin(0, 0); // Center the text on the image
+        
         // Position the image and text as needed
-        // Adjust the coordinates to place them correctly on the image
-        popupImage.setPosition(0, 0);
-        popupText.setPosition(0, popupImage.height / 2 + 20); // Adjust the Y position as needed
-    
-        // Create a close button
-        const closeButton = this.add.text(180, 80, 'Close', {
-            fontSize: '24px',
-            color: '#ffffff',
-            backgroundColor: '#333333',
-            padding: { x: 10, y: 5 },
-        }).setInteractive();
+        TaskDescriptionText.setPosition(-popupImage.width, -popupImage.height );
+        this.popupContainer.add(TaskDescriptionText);
+
+        // Create a close button using the 'taskCard' image
+        const closeButton = this.add.image(
+            popupImage.width + 10, // Adjust the X position for the bottom right corner
+            popupImage.height , // Adjust the Y position for the bottom right corner
+            'taskCard'
+        ).setScale(0.25); // Adjust the scale as needed
+
+        closeButton.setOrigin(1, 1); // Set the origin to the bottom right corner
+        closeButton.setInteractive();
+
+        // Add "Exit" text to the close button
+        const exitText = this.add.text(
+            closeButton.x - 100, // Adjust the X position for text placement
+            closeButton.y - 55, // Adjust the Y position for text placement
+            'Exit',
+            {
+                fontSize: '24px',
+                color: '#ffffff',
+            }
+        );
+
         closeButton.on('pointerdown', () => {
             // Close the popup when the close button is clicked
             this.closePopup();
         });
-        this.popupContainer.add(closeButton);
+
+        // Add the close button and text to the popup container
+        this.popupContainer.add([closeButton, exitText]);
+        
     }
       
     
