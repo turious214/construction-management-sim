cedb2fac75a39633a90f260b0c8daa2b35ef3d67
Luis Hebrero <hebrero.luis@gmail.com>
2023-09-22 23:31:08 +1000

    functionality improvement -- change in task class

--- src/scene/TaskGrid.ts
+++ src/scene/TaskGrid.ts
@@ -4,22 +4,7 @@
 - function returning a 2 dimensional array specifying where each task should be placed in the grid (Done)
 */
 
-/************************ TASK CLASS ********************************/
-//Task class with int id and a list of next tasks
-class Task {
-    id: number;
-    nextTasks: Task[];
-
-    constructor(id: number) {
-        this.id = id;
-        this.nextTasks = [];
-    }
-
-    addNextTask(task: Task) {
-        this.nextTasks.push(task);
-    }
-}
-/**********************************************************************/
+import Task from "../gameobject/Task.ts";
 
 /************************UTILITY FUNCTIONS********************************/
 
@@ -49,6 +34,7 @@ function longestChainHelper(task: Task): number {
 // @ts-ignore
 function maxNumOfTheSameDegree(tasks: Task[]): number {
     return 6;
+
 }
 
 /**********************************************************************/
@@ -90,8 +76,8 @@ function allocateTasks(tasks: Task[], current_x: number, current_y: Row, grid: n
     let i = 0
     let saved_y = current_y.value
     tasks.forEach((task: Task) => {
-        console.log(current_y.value, current_x , '--->' + task.id);
-        grid[current_y.value][current_x] = task.id;
+        console.log(current_y.value, current_x , '--->' + task.taskID);
+        grid[current_y.value][current_x] = task.taskID;
         if (task.nextTasks.length > 0) {
             allocateTasks(task.nextTasks, current_x + 1, current_y, grid);
         }
@@ -105,30 +91,30 @@ function allocateTasks(tasks: Task[], current_x: number, current_y: Row, grid: n
 
 /************************ SIMPLE TEST **********************************/
 
-const task1 = new Task(1);
-const task2 = new Task(2);
-const task3 = new Task(3);
-const task4 = new Task(4);
-const task5 = new Task(5);
-const task6 = new Task(6);
-const task7 = new Task(7);
-const task8 = new Task(8);
-const task9 = new Task(9);
-const task10 = new Task(10);
-const task11 = new Task(11);
-const task12 = new Task(12);
+const task1 = new Task(1, "dummy task", null);
+const task2 = new Task(2, "dummy task", null);
+const task3 = new Task(3, "dummy task", null);
+const task4 = new Task(4, "dummy task", null);
+const task5 = new Task(5, "dummy task", null);
+const task6 = new Task(6, "dummy task", null);
+const task7 = new Task(7, "dummy task", null);
+const task8 = new Task(8, "dummy task", null);
+const task9 = new Task(9, "dummy task", null);
+const task10 = new Task(10, "dummy task", null);
+const task11 = new Task(11, "dummy task", null);
+const task12 = new Task(12, "dummy task", null);
 
-task1.addNextTask(task2);
-task1.addNextTask(task3);
-task2.addNextTask(task4);
-task2.addNextTask(task5);
-task3.addNextTask(task6);
-task3.addNextTask(task8);
-task3.addNextTask(task9);
-task4.addNextTask(task7);
-task7.addNextTask(task10);
-task6.addNextTask(task11);
-task3.addNextTask(task12);
+task1.nextTasks = task1.nextTasks.concat(task2);
+task1.nextTasks = task1.nextTasks.concat(task3);
+task2.nextTasks = task2.nextTasks.concat(task4);
+task2.nextTasks = task2.nextTasks.concat(task5);
+task3.nextTasks = task3.nextTasks.concat(task6);
+task3.nextTasks = task3.nextTasks.concat(task8);
+task3.nextTasks = task3.nextTasks.concat(task9);
+task4.nextTasks = task4.nextTasks.concat(task7);
+task7.nextTasks = task7.nextTasks.concat(task10);
+task6.nextTasks = task6.nextTasks.concat(task11);
+task3.nextTasks = task3.nextTasks.concat(task12);
 
 const tasks = [task1];
 console.log(getGridSize(tasks)) //Should return 5, 6
