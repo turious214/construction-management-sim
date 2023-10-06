4c392a455d2cdeee1abe9027cc6c7e66473bc423
Eric Tran <etra8011@uni.sydney.edu.au>
2023-10-06 20:59:09 +1100

    Copied code from ContractorsScene to MaterialsScene

--- src/scene/HUDScene.ts
+++ src/scene/HUDScene.ts
@@ -15,13 +15,13 @@ export default class HUDScene extends Phaser.Scene {
         this.currentScene = 'ProjectScene';
     }
 
-    preload() {
+    preload(): void {
         // this.control = new Control(this);
         this.load.image('button1Normal', 'assets/buttons/button_normal.png');
         this.load.image('button1Hover', 'assets/buttons/button_hover.png');
     }
 
-    create() {
+    create(): void {
 
         const INIT_MAIN_UI_X = 200;
         const INIT_MAIN_UI_Y = 50;
@@ -30,29 +30,29 @@ export default class HUDScene extends Phaser.Scene {
         // projectButton
         // create button and add to scene
         
-        const projectButton = new CustomButton(this, INIT_MAIN_UI_X, INIT_MAIN_UI_Y, 'button1Normal', 'button1Hover', 'Project', HUD_BUTTON_TEXT_SIZE);
+        const projectButton: CustomButton = new CustomButton(this, INIT_MAIN_UI_X, INIT_MAIN_UI_Y, 'button1Normal', 'button1Hover', 'Project', HUD_BUTTON_TEXT_SIZE);
         this.add.existing(projectButton);
         this.currentButton = projectButton;
         projectButton.setTextColor('#fcd498');
 
         // contractorsButton
-        const contractorsButton = new CustomButton(this, projectButton.x + projectButton.width, INIT_MAIN_UI_Y, 'button1Normal', 'button1Hover', 'Contractors', HUD_BUTTON_TEXT_SIZE);
+        const contractorsButton: CustomButton = new CustomButton(this, projectButton.x + projectButton.width, INIT_MAIN_UI_Y, 'button1Normal', 'button1Hover', 'Contractors', HUD_BUTTON_TEXT_SIZE);
         this.add.existing(contractorsButton);
         
         // materialsButton
-        const materialsButton = new CustomButton(this, contractorsButton.x + contractorsButton.width, INIT_MAIN_UI_Y, 'button1Normal', 'button1Hover', 'Materials', HUD_BUTTON_TEXT_SIZE);
+        const materialsButton: CustomButton = new CustomButton(this, contractorsButton.x + contractorsButton.width, INIT_MAIN_UI_Y, 'button1Normal', 'button1Hover', 'Materials', HUD_BUTTON_TEXT_SIZE);
         this.add.existing(materialsButton);
 
         // personnelButton
-        const personnelButton = new CustomButton(this, materialsButton.x + materialsButton.width, INIT_MAIN_UI_Y, 'button1Normal', 'button1Hover', 'Personnel', HUD_BUTTON_TEXT_SIZE);
+        const personnelButton: CustomButton = new CustomButton(this, materialsButton.x + materialsButton.width, INIT_MAIN_UI_Y, 'button1Normal', 'button1Hover', 'Personnel', HUD_BUTTON_TEXT_SIZE);
         this.add.existing(personnelButton);
 
         // ContractsButton
-        const contractButton = new CustomButton(this, personnelButton.x + personnelButton.width, INIT_MAIN_UI_Y, 'button1Normal', 'button1Hover', 'Contract', HUD_BUTTON_TEXT_SIZE);
+        const contractButton: CustomButton = new CustomButton(this, personnelButton.x + personnelButton.width, INIT_MAIN_UI_Y, 'button1Normal', 'button1Hover', 'Contract', HUD_BUTTON_TEXT_SIZE);
         this.add.existing(contractButton);
 
         // endWeekButton - change to dynamically allow different weeks
-        const endWeekButton = new CustomButton(this, contractButton.x + contractButton.width, INIT_MAIN_UI_Y, 'button1Normal', 'button1Hover', 'END WEEK 1', HUD_BUTTON_TEXT_SIZE);
+        const endWeekButton: CustomButton = new CustomButton(this, contractButton.x + contractButton.width, INIT_MAIN_UI_Y, 'button1Normal', 'button1Hover', 'END WEEK 1', HUD_BUTTON_TEXT_SIZE);
         this.add.existing(endWeekButton);
 
         projectButton.setInteractive()
